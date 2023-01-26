3454159b3f6a134aaf0b3aaa9e2ff6113a56ce77
Sunwoo <dlatjsdn205@Naver.com>
2023-01-26 21:55:39 +0900

    [CREATE]create vuewitter-vue3

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex">
+    <div class="w-1/4 bg-gray-200 h-screen">
+      <i class="fab fa-twitter"></i>
+    </div>
+
+    <div class="flex-1 bg-red-200">1</div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
